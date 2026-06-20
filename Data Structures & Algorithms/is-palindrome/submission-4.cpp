cd68702ef06716964fbd094d9d032b537bd86166
Uday Krishna Subramaniam <uday.subramaniam89@gmail.com>
2026-06-20 14:43:47 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+        
+            while((left < right) && !isalnum(s[left])){
+                left++;
+            }
+
+            while((left < right) && !isalnum(s[right])){
+                right--;
+            }
+        
+            char leftCharacter = tolower(s[left]);
+            char rightCharacter = tolower(s[right]);
+            
+            if(leftCharacter != rightCharacter){
+                //mismatch, not a palindrome
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        return true;        
+    }
+};

cd25a84391540f5f85b533d9881fcff6bfb8b816
Uday Krishna Subramaniam <uday.subramaniam89@gmail.com>
2026-06-23 17:43:57 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b){
+            return a.start < b.start;
+        });
+
+        for(int i = 1; i < intervals.size(); i++){
+            int previousEnd = intervals[i-1].end;
+            int currentStart = intervals[i].start;
+
+            if(currentStart < previousEnd){
+                return false;
+            }
+        }
+        return true;
+    }
+};

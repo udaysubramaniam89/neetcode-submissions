dd347e3ff084554069fe42bee1e307dda2010838
Uday Krishna Subramaniam <uday.subramaniam89@gmail.com>
2026-06-26 15:25:04 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26, 0);
+
+        for(char task : tasks){
+            count[task - 'A']++;
+        }
+
+        priority_queue<int> maxHeap;
+
+        for(int freq : count){
+            if(freq > 0){
+                maxHeap.push(freq);
+            }
+        }
+
+        queue<pair<int, int>> cooldown;
+        int time = 0;
+
+        while(!maxHeap.empty() || !cooldown.empty()){    
+            time++;
+            if(!maxHeap.empty()){
+                int freq = maxHeap.top();
+                maxHeap.pop();
+                freq--;
+
+                if(freq > 0){
+                    cooldown.push({freq, time + n});
+                }
+            }
+
+            if(!cooldown.empty() && cooldown.front().second == time){
+                maxHeap.push(cooldown.front().first);
+                cooldown.pop();
+            }
+        }
+        return time;
+    }
+};
